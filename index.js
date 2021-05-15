a9b34eca96c5fd1c2290a70bf4e35caf494aab09
Jason carrazco <carrazcojason10@gmail.com>
2021-05-14 23:25:37 -0500

    added vew options to index

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const consoleTtable = require('console.table');
 
+const viewOptions = [
+  "View Departments",
+  "View Roles",
+  "View Employees",
+  "Update Employee",
+  "exit"
+];
+
+
