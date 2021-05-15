1c35e95841a378cacb6757c990c74dd2fa5e8bd3
Jason carrazco <carrazcojason10@gmail.com>
2021-05-14 23:07:17 -0500

    seeds info

--- seeds.sql
+++ seeds.sql
@@ -9,6 +9,21 @@ VALUES ("Engineering");
 INSERT into department (name)
 VALUES ("Finance");
 INSERT into department (name)
-VALUES ("Legal');
+VALUES ("Legal");
 INSERT into department (name)
 VALUES ("Manager");
+
+select * from department;
+
+INSERT into role (title, salary, department_id)
+VALUES ("Sales Lead", 33000, 1);
+INSERT into role (title, salary, department_id)
+VALUES ("Salesperson", 28000, 1);
+INSERT into role (title, salary, department_id)
+VALUES ("Lead Engineer", 47000 2);
+INSERT into role (title, salary, department_id)
+VALUES ("Accountant", 52500, 3);
+INSERT into role (title, salary, department_id)
+VALUES ("Legal", 51000, 4);
+INSERT into role (title, salary, department_id)
+VALUES ("Manager", 65000, 5);
